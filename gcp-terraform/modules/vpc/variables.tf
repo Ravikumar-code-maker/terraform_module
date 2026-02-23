bbde1d2376ec885279798513d27ac099b143e0bb
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-02-23 18:05:08 +0530

    Update variables.tf

--- gcp-terraform/modules/vpc/variables.tf
+++ gcp-terraform/modules/vpc/variables.tf
@@ -1,3 +1,10 @@
 variable "name" {}
 variable "region" {}
 variable "subnet_cidr" {}
+
+variable "internal_cidr" {
+  description = "Internal CIDR range to allow full internal traffic"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
