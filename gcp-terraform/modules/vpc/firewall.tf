30671ed2aea9b493f87d5c1bfba8474dbc21c4b6
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-02-23 17:39:38 +0530

    Update firewall.tf

--- gcp-terraform/modules/vpc/firewall.tf
+++ gcp-terraform/modules/vpc/firewall.tf
@@ -11,7 +11,7 @@ resource "google_compute_firewall" "allow_ssh" {
 
 # Allow internal traffic between all subnets
 
-resource "google_compute-firewall" "allow-http_https" {
+resource "google_compute-firewall" "allow_http_https" {
   name    = "allow-http-https"
   network = google_compute_network.vpc.id
 
@@ -23,5 +23,22 @@ resource "google_compute-firewall" "allow-http_https" {
 }
 
 # Allow internal traffic between all subnets
+resource "google_compute_firewall" "allow_internal" {
+  name    = "allow-internal"
+  network = google_compute_network.vpc.id
 
+  allow {
+    protocol = "icmp"
+  }
+   allow {
+     protocol = "tcp"
+     ports    = ["0-65535"]
+   }
+   allow {
+     protocol = "udp"
+     ports    = ["0-65535"]
+   }
+     # Replace with your internal CIDR range if needed
+  source_ranges = [var.internal_cidr]
+}
 
